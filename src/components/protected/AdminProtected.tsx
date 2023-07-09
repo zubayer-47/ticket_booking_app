efaa35b0f5e324355ea97e2e3268b0d8d4a52dfd
ZubayerJS <zubayerjs.dev@gmail.com>
2023-07-09 22:03:05 +0600

    AdminProtected file modified

---
+++ src/components/protected/AdminProtected.tsx
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { Context } from '../../contexts/Context';
+
+export default function AdminProtected() {
+    const { state } = useContext(Context);
+
+    return (
+        state.user.role === 'admin' ? <Outlet /> : <Navigate to='/profile' />
+    )
+}
