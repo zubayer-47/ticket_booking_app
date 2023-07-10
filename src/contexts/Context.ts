3384c2856972045310379656ea697d2538cc255e
ZubayerJS <zubayerjs.dev@gmail.com>
2023-07-10 13:53:50 +0600

    update Context

--- src/contexts/Context.ts
+++ src/contexts/Context.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-import { Store } from "../constants/context-constant";
+import { InitialStateType } from "../constants/context-constant";
 
-export const Context = createContext<Store>({} as Store);
+export const Context = createContext<InitialStateType>({} as InitialStateType);
