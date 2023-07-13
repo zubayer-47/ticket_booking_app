8d6cbb5e273637f95933a946ae136ad70fc3cedf
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-13 22:07:55 +0600

    main brand comp

--- src/views/brand/index.tsx
+++ src/views/brand/index.tsx
@@ -2,9 +2,5 @@ import { Outlet } from 'react-router-dom'
 
 export default function Brand() {
 
-    return <div>
-        <h1>Create Your Brand</h1>
-
-        <Outlet />
-    </div>
+    return <Outlet />
 }
