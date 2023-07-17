cde63515c0cde92ff24b27f86fb3d102b1fa8cc9
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-17 21:26:15 +0600

    fix all of errors in Select component

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -1,5 +1,3 @@
-import { useContext, useEffect, useRef } from "react";
-import { Context } from "../../contexts/Context";
 import { IdNameBrandLocationFromType, ToType } from "../../types/state.types";
 import Label from "../Inputs/Label";
 
@@ -13,17 +11,7 @@ type SelectProps = {
 }
 
 // give it a detailed name later
-// eslint-disable-next-line no-empty-pattern
 export default function Select({ name, label, state, handleSelected, defaultOptionValue, empty }: SelectProps) {
-    const selectRef = useRef(null);
-    const { state: appState } = useContext(Context);
-
-
-    useEffect(() => {
-
-        console.log(appState.from.selectedFromId)
-    }, [appState.from.selectedFromId])
-
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         console.log(e.target.value)
     }
@@ -36,11 +24,9 @@ export default function Select({ name, label, state, handleSelected, defaultOpti
     }
 
     return (
-        // <div className="w-full space-y-3">
         <div>
             <Label text={label} isRequired />
             <select
-                ref={selectRef}
                 name={name}
                 className="bg-white outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                 onClick={handleClick}
@@ -49,19 +35,8 @@ export default function Select({ name, label, state, handleSelected, defaultOpti
                 <option>{defaultOptionValue ?? "Choose From"}</option>
                 {state.length ? state.map((fr) => (
                     <option key={fr.id} value={`${fr.name} ${fr.id}`}>{fr.name}</option>
-                )) : (<option>{empty ?? "No From Exist"}</option>)}
+                )) : (<option value='none'>{empty ?? "No From Exist"}</option>)}
             </select>
         </div>
-
-        // {/* <div>
-        //     <Label text="To" isRequired />
-        //     <select name="to" className="bg-white outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5">
-        //         <option>Choose From</option>
-        //         {state.to.length ? state.to.map((v) => (
-        //             <option key={v.locationID} value={`${v.locationName} ${v.locationID}`} id="nothing">{v.locationName}</option>
-        //         )) : (<option>No Destination Exist</option>)}
-        //     </select>
-        // </div> */}
-        // </div>
     )
 }
