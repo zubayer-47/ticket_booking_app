37fcca4afdde95e8e1330c2d84cf33e542cc602a
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-21 00:19:46 +0600

    add a few types

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -1,7 +1,10 @@
 import React from "react";
 
 export type InputType = React.ChangeEvent<HTMLInputElement>;
+export type InputHandlerType = React.ChangeEventHandler<HTMLInputElement>;
+export type SelectHandler = React.ChangeEventHandler<HTMLSelectElement>;
 export type ClickType = React.MouseEventHandler<HTMLInputElement>;
 export type FormType = React.FormEvent<HTMLFormElement>;
 export type ButtonHandler = React.MouseEventHandler<HTMLButtonElement>;
 export type ClickHandler = React.MouseEventHandler<HTMLElement>;
+export type InputHtmlType = "text" | "email" | "number";
