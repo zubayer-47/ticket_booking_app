eb336ef8d31fbc7d370e80b202e8e80611efe3e6
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-21 00:19:17 +0600

    modified error page

--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -25,7 +25,6 @@ class ErrorBoundary extends Component<Props, State> {
   public render() {
     if (this.state.hasError) {
       return (
-        // <NotFound />
         <div className='h-screen w-full flex place-items-center justify-center items-center'>
           <div className='w-full text-center mx-3 p-4 bg-transparent rounded-xl select-none'>
             <p className='flex justify-center items-center text-3xl md:text-5xl lg:text-7xl gap-2 tracking-widest font-bold text-red-500'>OOPS ERROR!!</p>
