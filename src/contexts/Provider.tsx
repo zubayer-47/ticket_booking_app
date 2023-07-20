8e8bf3b43b083efce27712e6141d29c639c38afd
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-21 00:19:31 +0600

    modified

--- src/contexts/Provider.tsx
+++ src/contexts/Provider.tsx
@@ -46,57 +46,52 @@ function reducer(state: InitialStateType, action: ActionType): InitialStateType 
 export default function Provider({ children }: { children: ReactNode }) {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const setLocalStorage = (token: string) => {
-        const _token = localStorage.getItem("_token")
-        // console.log(_token?.length);
-
-        if (!_token?.length && !!token.length) {
-            localStorage.setItem("_token", JSON.stringify(token));
-        }
-    }
-
     // handle error boundary
     useEffect(() => {
+        dispatch({ type: "LOADING", payload: true })
+
         const controller = new AbortController();
         const userToken = localStorage.getItem("_token");
 
         const fetchUser = async () => {
-            try {
-                const res = await api.get('/users/me', {
-                    headers: {
-                        Authorization: userToken
-                    },
-                    signal: controller.signal
-                });
+            if (userToken) {
+                try {
+                    const res = await api.get('/users/me', {
+                        headers: {
+                            Authorization: userToken
+                        },
+                        signal: controller.signal
+                    });
 
-                setLocalStorage(res.data?.token)
-                dispatch({
-                    type: "ADD_USER", payload: {
-                        name: res.data?.fullname,
-                        email: res.data?.email,
-                        authenticated: true,
-                        role: res.data?.role,
-                        token: res.data?.token,
-                        ticket: res.data?.ticket
+                    // setLocalStorage(res.data?.token)
+                    dispatch({
+                        type: "ADD_USER", payload: {
+                            name: res.data?.fullname,
+                            email: res.data?.email,
+                            authenticated: true,
+                            role: res.data?.role,
+                            token: res.data?.token,
+                            ticket: res.data?.ticket
+                        }
+                    });
+                } catch (error) {
+                    if (axios.isAxiosError(error)) {
+                        // const message = error.response?.data?.message;
+                        console.log(error)
+                        // handle error manually
+                        // if (error.status)
                     }
-                });
-            } catch (error) {
-                if (axios.isAxiosError(error)) {
-                    // const message = error.response?.data?.message;
-                    // handle error manually
-                    // if (error.status)
-                    console.log(error)
                 }
-            }
 
-            dispatch({ type: "LOADING", payload: false })
+                // dispatch({ type: "LOADING", payload: false })
+            }
+            // dispatch({ type: "LOADING", payload: false })
         }
 
-        dispatch({ type: "LOADING", payload: true })
         fetchUser();
 
         return () => controller.abort();
-    }, [state.user.authenticated, state.isLoading]);
+    }, []);
 
     const value = useMemo(() => ({ state: { ...state }, dispatch }), [state, dispatch]);
 
