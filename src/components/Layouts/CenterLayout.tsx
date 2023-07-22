2e83b24921a00c62f0897e13ec94d6a9d455b89f
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-22 15:19:45 +0600

    nothing fancy changes

--- src/components/Layouts/CenterLayout.tsx
+++ src/components/Layouts/CenterLayout.tsx
@@ -8,11 +8,10 @@ type PropType = {
 
 const CenterLayout: React.FC<PropType> = ({ children, noWidth, smWidth }) => {
 	return (
-		<div className='h-full flex items-center justify-center'>
+		<div className='h-full flex justify-center place-items-center'>
 			<div
-				className={`w-full h-full mx-3 md:mx-0 p-4 bg-white rounded-xl shadow-md ${
-					!noWidth && 'md:w-11/12 lg:w-4/5'
-				} ${smWidth && 'md:max-w-md lg:max-w-lg'}`}
+				className={`w-full h-full mx-3 md:mx-0 p-4 bg-white rounded-xl shadow-md ${!noWidth && 'md:w-11/12 lg:w-4/5'
+					} ${smWidth && 'md:max-w-md lg:max-w-lg'}`}
 			>
 				{children}
 			</div>
