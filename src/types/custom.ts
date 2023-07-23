b7df32b0eb4c1d7c7c7619034e71897b74433fb5
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-23 17:37:19 +0600

    create select and input change event in one variable

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -8,3 +8,6 @@ export type FormType = React.FormEvent<HTMLFormElement>;
 export type ButtonHandler = React.MouseEventHandler<HTMLButtonElement>;
 export type ClickHandler = React.MouseEventHandler<HTMLElement>;
 export type InputHtmlType = "text" | "email" | "number" | "password";
+export type InputSelectChangeType =
+  | React.ChangeEvent<HTMLSelectElement>
+  | React.ChangeEvent<HTMLInputElement>;
