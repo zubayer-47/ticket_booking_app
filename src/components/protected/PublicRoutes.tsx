be10307486ebd6350c340cbffd0525d946631360
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-24 18:12:08 +0600

    Public route fixes

--- src/components/protected/PublicRoutes.tsx
+++ src/components/protected/PublicRoutes.tsx
@@ -3,11 +3,11 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { Context } from '../../contexts/Context';
 
 export default function PublicRoutes() {
-    const { state: { user } } = useContext(Context)
+    const { state } = useContext(Context)
     // const location = useLocation();
 
-    return !user.authenticated ?
+    return !state.authenticated ?
         <Outlet />
-        : user.role === 'admin' ? <Navigate to='/dashboard' />
+        : state.user.role === 'admin' ? <Navigate to='/dashboard' />
             : <Navigate to='/profile' />
 }
