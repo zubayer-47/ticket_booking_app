637ec220e5bb3cdf49b092cbf001ffd55041ca0f
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-24 18:10:50 +0600

    add few props

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,14 +1,15 @@
 
 type ErrorProps = {
     error: string;
+    classNames?: string
 }
 
-export default function Error({ error }: ErrorProps) {
+export default function Error({ error, classNames }: ErrorProps) {
     return (
         // <p className="mt-2 text-center">
         <>
             {!!error && (
-                <span className='mb-2 text-sm text-center text-red-400 tracking-wider'>
+                <span className={` text-red-400 tracking-wider ${classNames ? classNames : "mb-2 text-sm text-center"}`}>
                     {error}
                 </span>
             )}
