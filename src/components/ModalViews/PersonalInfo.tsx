0ad1e2745b0fc5f28feed790bd7f397589f6225b
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-25 15:30:01 +0600

    personal information design changed

--- src/components/ModalViews/PersonalInfo.tsx
+++ src/components/ModalViews/PersonalInfo.tsx
@@ -1,12 +1,44 @@
 
 
-import { FormEvent } from 'react';
-import Input, { CheckBox } from '../Inputs/Inputs';
-import Label from '../Inputs/Label';
+import { FormEvent, useState } from 'react';
+import CommonInput from '../Inputs/CommonInput';
+import { CheckBox } from '../Inputs/Inputs';
+import CommonSelect from '../Selects/CommonSelect';
 import { Gender } from '../Selects/Selects';
 
+export interface PassengerDetailsType {
+    name: string;
+    gender: string;
+    email: string;
+    age: number;
+    mobile: number;
+    boarding_point: string;
+    dropping_point: string;
+    isAgree: boolean;
+}
+
+const boarding_point_list = [
+    { id: 1, name: "Sylhet Kadamtoli Bus Stand (5:00PM)" },
+    { id: 2, name: "Humayun Cottor (5:00PM)" },
+]
+
+const dropping_point_list = [
+    { id: 1, name: "Saydabad" },
+    { id: 2, name: "Chittagong Road" },
+]
 
 export default function PersonalInfo() {
+    const [passengerDetails, setPassengerDetails] = useState<PassengerDetailsType>({
+        name: "",
+        gender: "",
+        email: "",
+        age: 0,
+        boarding_point: "",
+        dropping_point: "",
+        mobile: 0,
+        isAgree: false,
+    })
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
@@ -24,41 +56,109 @@ export default function PersonalInfo() {
         //     dropping_point: formData.get('dropping_point'),
         // }
 
+        console.log(passengerDetails)
+
     }
     return (
         <>
-            <form className="mt-3 lg:mt-5 border p-2 rounded-md space-y-2" onSubmit={handleSubmit}>
+            <form className="mt-3 lg:mt-5 border p-2 rounded-md" onSubmit={handleSubmit}>
                 <p className="text-lg">Personal Information:</p>
-                <div className="grid grid-cols-2 gap-2">
-                    <Input id='name' name='name' isRequired label="Name" placeholder="your name" />
-                    <div className="flex justify-center items-center mt-5">
-                        <Gender />
-                    </div>
+                <div className="flex justify-between items-center gap-2">
+                    <CommonInput
+                        name='name'
+                        placeholder='your name'
+                        label='Name'
+                        required
+                        value={passengerDetails.name}
+                        change={e => setPassengerDetails(prev => ({
+                            ...prev,
+                            name: e.target.value,
+                        }))}
+                        type='text'
+                        inputClasses='border w-full'
+                        classNames='w-full'
+                    />
+                    <Gender
+                        value={passengerDetails.gender}
+                        setValue={setPassengerDetails}
+                    />
                 </div>
 
-                <Input id='email' name='email' isRequired label="email" type="email" placeholder="example@zubayer.com" />
+                <CommonInput
+                    name='email'
+                    placeholder='example@zubayer.com'
+                    label='Email'
+                    required
+                    value={passengerDetails.email}
+                    change={e => setPassengerDetails(prev => ({
+                        ...prev,
+                        email: e.target.value,
+                    }))}
+                    type='email'
+                    inputClasses='border'
+                />
 
                 <div className="grid grid-cols-2 gap-2">
-                    <Input id='age' name='age' isRequired label="Age" placeholder="18" type="number" />
-                    <Input id='mobile' name='mobile' isRequired label="Mobile" placeholder="01600000000" type="tel" />
+                    <CommonInput
+                        name='age'
+                        placeholder='00'
+                        label='Age'
+                        required
+                        value={passengerDetails.age}
+                        change={e => setPassengerDetails(prev => ({
+                            ...prev,
+                            age: +e.target.value,
+                        }))}
+                        type='number'
+                        inputClasses='border'
+                    />
+                    <CommonInput
+                        name='mobile'
+                        placeholder='01600000000'
+                        label='Mobile'
+                        required
+                        value={passengerDetails.mobile}
+                        change={e => setPassengerDetails(prev => ({
+                            ...prev,
+                            mobile: +e.target.value,
+                        }))}
+                        type='number'
+                        inputClasses='border'
+                    />
                 </div>
 
-                <div className="grid grid-cols-2 gap-2">
-                    <div className="flex justify-center items-center">
-                        <Label text="Boarding Point" isRequired />
-                        <select name='boarding_point' className="block w-full p-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-blue-500 focus:border-blue-500" required>
-                            <option selected>Select a Boarding Point</option>
-                            <option value="kadamtoli">Sylhet Kadamtoli Bus Stand (5:00PM)</option>
-                            <option value="humayun_cottor">Humayun Cottor (5:00PM)</option>
-                        </select>
-                    </div>
-                    <div className="flex justify-center items-center">
-                        <Label text="Dropping Point" isRequired />
-                        <select name='dropping_point' className="block w-full p-1 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-blue-500 focus:border-blue-500" required>
-                            <option selected>Select a Dropping Point</option>
-                            <option value="kadamtoli">Sayedabad (11:00PM)</option>
-                        </select>
-                    </div>
+                <div className="flex justify-center items-center gap-2">
+                    <CommonSelect
+                        defSelectName="Select Boarding Point"
+                        label="Boarding Point"
+                        name="boarding_point"
+                        options={boarding_point_list}
+                        change={(e) => setPassengerDetails(prev => ({
+                            ...prev,
+                            boarding_point: e.target.value,
+                        }))}
+                        value={passengerDetails.boarding_point}
+                        valueInName
+                        required
+                        selectClasses="bg-white border py-3.5 px-2"
+                        classNames="w-full"
+                    />
+
+                    <CommonSelect
+                        defSelectName="Select Dropping Point"
+                        label="Dropping Point"
+                        name="dropping_point"
+                        options={dropping_point_list}
+                        change={(e) => setPassengerDetails(prev => ({
+                            ...prev,
+                            dropping_point: e.target.value,
+                        }))}
+                        value={passengerDetails.dropping_point}
+                        valueInName
+                        required
+                        selectClasses="bg-white border py-3.5 px-2"
+                        classNames="w-full"
+                    />
                 </div>
 
                 <div className='flex justify-center flex-col items-center pt-3'>
@@ -67,6 +167,8 @@ export default function PersonalInfo() {
                         linkText='TERMS AND CONDITIONS'
                         to='/'
                         text='I AGREE TO ALL THE'
+                        isAgree={passengerDetails.isAgree}
+                        setAgree={setPassengerDetails}
                     />
                     <button
                         type="submit"
