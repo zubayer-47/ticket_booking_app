144cb95501bdafbfb4fe52d16f38b8784f53b7ef
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-25 23:05:52 +0600

    add a few constant

--- src/constants/context-constant.ts
+++ src/constants/context-constant.ts
@@ -4,4 +4,14 @@ export const enum Action {
   "REMOVE_USER",
   "UPDATE_USER",
   "LOADING",
+
+  // searchProds
+  "ADD_PRODS",
+  "REMOVE_PRODS",
+  "ADD_PRODS_STATUS",
+
+  // passenger personal information
+  "ADD_PASSENGER_INFO",
+  "REMOVE_PASSENGER_INFO",
+  "PASSENGER_LOADING",
 }
