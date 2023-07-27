9c26bedd4794380306e449775361a6c5c2823722
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-27 16:59:02 +0600

    organized profile page

--- src/views/profile/Profile.tsx
+++ src/views/profile/Profile.tsx
@@ -1,84 +1,43 @@
-import { useState } from "react";
-import Input, { PasswordInput } from "../../components/Inputs/Inputs";
-import PageLayout from "../../components/Layouts/PageLayout";
-import { Gender } from "../../components/ModalViews/Gender";
-import { FormType } from "../../types/custom";
+import { useContext } from "react";
+import { FiUser } from "react-icons/fi";
+import CommonInput from "../../components/Inputs/CommonInput";
+import CenterLayout from "../../components/Layouts/CenterLayout";
+import { Context } from "../../contexts/Context";
 
 export default function Profile() {
-    const [submitting, setSubmitting] = useState(false);
+    const { state } = useContext(Context)
 
-    const handleSubmit = (e: FormType) => {
-        e.preventDefault();
-
-
-        setSubmitting(true);
-    }
     return (
-        <PageLayout>
-            <form className="space-y-3" onSubmit={handleSubmit}>
-                <Input
-                    id="email"
-                    label="Email"
-                    name="email"
-                    isRequired
-                    placeholder="example@zubayer.com"
-                    type="email"
-                    defaultSize
-                />
-                <PasswordInput />
-                <Input
-                    id="name"
-                    label="Name"
-                    name="name"
-                    isRequired
-                    placeholder="Your Name"
-                    defaultSize
-                />
-                <Input
-                    id="address"
-                    label="Address"
-                    name="address"
-                    isRequired
-                    placeholder="Address"
-                    defaultSize
-                />
-                <Gender />
-                <Input
-                    id="age"
-                    label="Age"
-                    name="age"
-                    isRequired
-                    placeholder="Age"
-                    defaultSize
-                    type="number"
-                />
-                <Input
-                    id='mobile'
-                    name='mobile'
-                    isRequired
-                    label="Mobile"
-                    placeholder="01600000000"
-                    defaultSize
-                    type="tel"
-                />
+        <CenterLayout smWidth>
 
+            <div className="w-full flex justify-center items-center pb-8">
+                <FiUser className='text-8xl text-gray-50 rounded-full bg-emerald-500 cursor-pointer p-1' />
+            </div>
 
-                {/* <div className="relative items-center block max-w-sm p-6 bg-white border border-gray-100 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-800 dark:hover:bg-gray-700"> */}
-                <button
-                    type="submit"
-                    className="relative items-center block bg-emerald-500 text-white mt-2 active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 overflow-hidden"
-                >
-                    <p>Update</p>
-                    {submitting ? (<div role="status" className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-gray-100/60">
-                        <svg aria-hidden="true" className="w-7 h-7 text-gray-100 animate-spin fill-emerald-400 bg-gray-100/5" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" /><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" /></svg>
-                        <span className="sr-only">Loading...</span>
-                    </div>) : null}
-                </button>
-
-                {/* </div> */}
-
-
-            </form>
-        </PageLayout>
+            <CommonInput
+                name="email"
+                placeholder="example@zubayer.com"
+                type="email"
+                change={() => {
+                    console.log()
+                }}
+                value={state.user.email}
+                label="Email"
+                required
+                isLoading
+            />
+            <CommonInput
+                name="name"
+                placeholder="your full name"
+                type="text"
+                change={() => {
+                    console.log()
+                }}
+                value={state.user.name}
+                label="Full Name"
+                required
+                isLoading
+            />
+        </CenterLayout>
     )
 }
