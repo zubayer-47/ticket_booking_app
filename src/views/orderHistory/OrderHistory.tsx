689a884fe7e1bcf83f9e9bff01c5dc07227c1945
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-27 16:58:46 +0600

    modified order history

--- src/views/orderHistory/OrderHistory.tsx
+++ src/views/orderHistory/OrderHistory.tsx
@@ -1,6 +1,17 @@
+import { useContext } from "react";
+import CenterLayout from "../../components/Layouts/CenterLayout";
+import { Context } from "../../contexts/Context";
 
 export default function OrderHistory() {
+    const { state } = useContext(Context);
+
     return (
-        <div>OrderHistory</div>
+        <CenterLayout smWidth>
+            <ul className="space-y-2">
+                {state.user.ticket.map(t => (
+                    <li className={`border-b`} key={t.id}>{t.seatName}</li>
+                ))}
+            </ul>
+        </CenterLayout>
     )
 }
