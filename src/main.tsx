6eaae8fa168d19e74722685e9bef8f83844e1f24
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-28 18:20:51 +0600

    comment strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import ErrorBoundary from './components/errors/ErrorBoundary.tsx'
-import Provider from './contexts/Provider.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import ErrorBoundary from './components/errors/ErrorBoundary.tsx';
+import Provider from './contexts/Provider.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <Provider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </ErrorBoundary>
-  </React.StrictMode >
+  // <React.StrictMode>
+  <ErrorBoundary>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </ErrorBoundary>
+  // </React.StrictMode>
 )
