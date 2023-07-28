19425b6d913d5134b47843c93a66a35cfcf3b84c
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-28 20:55:21 +0600

    working

--- src/components/ModalViews/BusSeats.tsx
+++ src/components/ModalViews/BusSeats.tsx
@@ -1,16 +1,26 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { FiLifeBuoy } from 'react-icons/fi';
-import useFilterDuplicateSeats from '../../hooks/useFilterDuplicateSeats';
+import { v4 } from 'uuid';
 import { TicketType } from '../../types/state.types';
 import api from '../../utils/axios';
 import filterTwoArr from '../../utils/filterTwoArr';
 
-const seat2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const seat3: TicketType[][] = [
+    [{ id: "1", seatName: "A-1" }, { id: "2", seatName: "A-2" }, { id: "3", seatName: "A-3" }, { id: "4", seatName: "A-4" }],
+    [{ id: "1", seatName: "B-1" }, { id: "2", seatName: "B-2" }, { id: "3", seatName: "B-3" }, { id: "4", seatName: "B-4" }],
+    [{ id: "1", seatName: "C-1" }, { id: "2", seatName: "C-2" }, { id: "3", seatName: "C-3" }, { id: "4", seatName: "C-4" }],
+    [{ id: "1", seatName: "D-1" }, { id: "2", seatName: "D-2" }, { id: "3", seatName: "D-3" }, { id: "4", seatName: "D-4" }],
+    [{ id: "1", seatName: "E-1" }, { id: "2", seatName: "E-2" }, { id: "3", seatName: "E-3" }, { id: "4", seatName: "E-4" }],
+    [{ id: "1", seatName: "F-1" }, { id: "2", seatName: "F-2" }, { id: "3", seatName: "F-3" }, { id: "4", seatName: "F-4" }],
+    [{ id: "1", seatName: "G-1" }, { id: "2", seatName: "G-2" }, { id: "3", seatName: "G-3" }, { id: "4", seatName: "G-4" }],
+    [{ id: "1", seatName: "H-1" }, { id: "2", seatName: "H-2" }, { id: "3", seatName: "H-3" }, { id: "4", seatName: "H-4" }],
+    [{ id: "1", seatName: "I-1" }, { id: "2", seatName: "I-2" }, { id: "3", seatName: "I-3" }, { id: "4", seatName: "I-4" }],
+]
 
 export default function BusSeats({ prodID }: { prodID: string }) {
-    const [seats, setSeats] = useState<TicketType[]>([])
-    const { seats: filteredSeats } = useFilterDuplicateSeats(seats);
+    const [_, setSeats] = useState<TicketType[]>([])
+    // const { seats: filteredSeats } = useFilterDuplicateSeats(seats);
 
     const demo = filterTwoArr()
     console.log(demo)
@@ -32,7 +42,6 @@ export default function BusSeats({ prodID }: { prodID: string }) {
 
         return () => controller.abort()
     }, [prodID]);
-
     return (<div className='p-2'>
         <table className="border border-collapse w-full">
             <thead>
@@ -64,16 +73,18 @@ export default function BusSeats({ prodID }: { prodID: string }) {
 
         {/* seats */}
         <div className="grid grid-cols-bus_seats gap-2 mt-5 w-full overflow-auto">
-            {seat2.map((v) => (<div className="flex justify-between items-center gap-5 lg:gap-12" key={v}>
-                <div className="flex gap-2">
-                    <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">A1</button>
-                    <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">A2</button>
+            {seat3.map((seatArr) => (
+                <div className="flex justify-between items-center gap-5 lg:gap-12" key={v4()}>
+                    <div className="flex gap-2">
+                        <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">{seatArr[0].seatName}</button>
+                        <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">{seatArr[1].seatName}</button>
+                    </div>
+                    <div className="flex gap-2">
+                        <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">{seatArr[2].seatName}</button>
+                        <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">{seatArr[3].seatName}</button>
+                    </div>
                 </div>
-                <div className="flex gap-2">
-                    <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">A1</button>
-                    <button className="bg-gray-100 text-gray-700 border border-gray-500 px-4 py-1 rounded-sm">A2</button>
-                </div>
-            </div>))}
+            ))}
         </div>
 
     </div>
