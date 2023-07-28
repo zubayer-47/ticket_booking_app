5fee650c807254506c5f6722ddfa7b7b6a7bef7f
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-28 22:03:40 +0600

    modifying

--- src/components/BusList.tsx
+++ src/components/BusList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useContext, useMemo, useState } from 'react';
 import { FiLayout } from 'react-icons/fi';
 import { Context } from '../contexts/Context';
 import { makeCoachName } from '../utils/coachName';
@@ -23,10 +23,6 @@ function BusList() {
 			return prod
 		}
 	}), [state.searchProds.fromID, state.searchProds.list])
-
-	useEffect(() => {
-		setShowModal(true);
-	}, [])
 
 	const handleView = (prodID: string) => {
 		setShowModal(true);
