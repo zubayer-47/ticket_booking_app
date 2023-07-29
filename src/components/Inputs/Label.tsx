765bffa6c599692d05b2011f1a29990e8d9d2b2a
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-29 22:21:00 +0600

    label modified

--- src/components/Inputs/Label.tsx
+++ src/components/Inputs/Label.tsx
@@ -6,7 +6,18 @@ type LabelProps = {
 }
 
 export default function Label({ text, id, isRequired = false }: LabelProps) {
-    return (
-        <label htmlFor={id} className={`block text-sm font-medium text-gray-900 mr-2 ${isRequired ? "after:content-['*'] after:text-red-600" : ""} capitalize`}>{text}</label>
-    )
+    return <label
+        htmlFor={id}
+        className='w-full flex items-center text-xs md:text-md font-bold tracking-wider px-1 text-gray-600'
+    >
+        <span>{text}</span>
+        {isRequired ? (
+            <span className='ml-0.5 -my-2 text-base font-bold text-red-400'>
+                *
+            </span>
+        ) : null}
+    </label>
+    // return (
+    //     <label htmlFor={id} className={`block text-sm font-medium text-gray-900 mr-2 ${isRequired ? "after:content-['*'] after:text-red-600" : ""} capitalize`}>{text}</label>
+    // )
 }
