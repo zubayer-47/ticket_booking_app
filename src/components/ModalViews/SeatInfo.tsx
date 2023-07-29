bd06109156b96df5d7e78d906a1bd921593f0268
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-29 22:22:34 +0600

    working with SeatInfo

--- src/components/ModalViews/SeatInfo.tsx
+++ src/components/ModalViews/SeatInfo.tsx
@@ -1,5 +1,6 @@
 import { CiSquareRemove } from 'react-icons/ci';
 import { v4 } from 'uuid';
+import { ModalStateType } from './TicketModal';
 
 const ticketList = [
     { id: 1, seat: 'D3', fare: 700, cancel: false },
@@ -10,44 +11,65 @@ const ticketList = [
 
 type SeatInfoProps = {
     seatNames: string[];
-    price: string | number
+    price: string | number;
+    setState: React.Dispatch<React.SetStateAction<ModalStateType>>;
+    charge?: number;
 }
 
-export default function SeatInfo({ seatNames, price }: SeatInfoProps) {
+export default function SeatInfo({ seatNames, price, setState, charge }: SeatInfoProps) {
+    const handleDelete = (name: string) => {
+        setState(prev => ({
+            ...prev,
+            seatNames: prev.seatNames.filter(seatName => seatName !== name)
+        }))
+    }
+
+    const fare = seatNames.length * +price;
+    const charge_fare = charge ? charge : 0
+    const net_fare = fare + charge_fare;
+
     return (
         <>
-            <table className="w-full border-collapse text-center">
-                <caption className="text-left text-lg font-bold">Seat Information:</caption>
-                <thead className="bg-emerald-500 text-gray-50">
-                    <tr>
-                        <th className="border border-slate-300 lg:p-1">Seat No</th>
-                        <th className="border border-slate-300 lg:p-1">Fare (TK)</th>
-                        <th className="border border-slate-300 lg:p-1">Remove</th>
-                    </tr>
-                </thead>
-
-                <tbody className="overflow-auto">
-                    {seatNames.map((ticket) => (
-                        <tr key={v4()} className="">
-                            <td className="border border-slate-300">{ticket}</td>
-                            <td className="border border-slate-300">{price}</td>
-                            <td className="text-red-500 border border-slate-300"><button><CiSquareRemove className="h-7 w-7" /></button></td>
+            <h1 className="text-left text-lg font-bold">Seat Information:</h1>
+            <div className={`max-h-[250px] overflow-auto `}>
+                <table className="w-full border-collapse text-center">
+                    <thead className="bg-emerald-500 text-gray-50">
+                        <tr>
+                            <th className="border border-slate-300 lg:p-1">Seat No</th>
+                            <th className="border border-slate-300 lg:p-1">Fare (TK)</th>
+                            <th className="border border-slate-300 lg:p-1">Remove</th>
                         </tr>
-                    ))}
-                </tbody>
+                    </thead>
+
+                    <tbody>
+                        {seatNames.map((seatName) => (
+                            <tr key={v4()}>
+                                <td className="border border-slate-300">{seatName}</td>
+                                <td className="border border-slate-300">{price}</td>
+                                <td className="text-red-500 border border-slate-300">
+                                    <button type='button' onClick={() => handleDelete(seatName)}>
+                                        <CiSquareRemove className="h-7 w-7" />
+                                    </button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
 
 
-            </table>
             <div className='grid grid-cols-3 items-center text-center border mt-4'>
                 <p className='border-r text-red-600 font-bold text-sm'>Total Seat: <span className='text-black'>{ticketList.length}</span></p>
                 <p className='border-r text-red-600 font-bold text-sm'>
-                    Fare: <span className='text-black'>{ticketList.length * ticketList[0].fare}</span> <br />
-                    <span className='text-red-600 font-bold text-sm'>Charge: <span className='text-black'>130tk</span></span>
+                    Fare: <span className='text-black'>{fare} TK</span> <br />
+                    {charge ? (
+                        <span className='text-red-600 font-bold text-sm'>Charge: <span className='text-black'>{charge_fare}</span></span>
+                    ) : null}
                 </p>
                 <p className='border-r'>
-                    <span className='text-red-600 font-bold text-sm'>Net Fare: <span className='text-black'>{ticketList.length * ticketList[0].fare + 130}tk </span></span >
+                    <span className='text-red-600 font-bold text-sm'>Net Fare: <span className='text-black'>{net_fare}tk </span></span >
                 </p>
             </div>
         </>
+
     )
 }
