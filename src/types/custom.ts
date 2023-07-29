78b5a98953eaf215f99d9d8a73984ca2923bbeb7
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-29 22:23:01 +0600

    modified input type

--- src/types/custom.ts
+++ src/types/custom.ts
@@ -7,7 +7,7 @@ export type ClickType = React.MouseEventHandler<HTMLInputElement>;
 export type FormType = React.FormEvent<HTMLFormElement>;
 export type ButtonHandler = React.MouseEventHandler<HTMLButtonElement>;
 export type ClickHandler = React.MouseEventHandler<HTMLElement>;
-export type InputHtmlType = "text" | "email" | "number" | "password";
+export type InputHtmlType = "text" | "email" | "number" | "password" | "mobile";
 export type InputSelectChangeType =
   | React.ChangeEvent<HTMLSelectElement>
   | React.ChangeEvent<HTMLInputElement>;
