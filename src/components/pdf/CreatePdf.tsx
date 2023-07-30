2042a030f41703684c5790cf6d5281c287d3e2c5
A B M Zubayer <zubayerabm47@gmail.com>
2023-07-30 20:48:13 +0600

    CreatePdf added

---
+++ src/components/pdf/CreatePdf.tsx
@@ -0,0 +1,9 @@
+
+export default function CreatePdf() {
+    return (
+        <div>create.pdf</div>
+    )
+}
+
+// https://pspdfkit.com/blog/2023/how-to-fill-a-pdf-form-in-react/
+// https://pspdfkit.com/blog/2019/create-pdfs-with-react/#:~:text=To%20create%20a%20PDF%2C%20you,root%20of%20a%20PDF%20file.
