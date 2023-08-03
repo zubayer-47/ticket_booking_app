f1299d2b2e5a0c5774bf5e7acc63f6945db0bedd
A B M Zubayer <zubayerabm47@gmail.com>
2023-08-04 00:29:04 +0600

    modified

--- src/components/ModalViews/PersonalInfo.tsx
+++ src/components/ModalViews/PersonalInfo.tsx
@@ -1,9 +1,13 @@
 
 
-import { FormEvent, useContext, useRef, useState } from 'react';
+import axios from 'axios';
+import { FormEvent, memo, useContext, useEffect, useRef, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useReactToPrint } from 'react-to-print';
 import { Context } from '../../contexts/Context';
+import { IdNameBrandLocationFromType } from '../../types/state.types';
+import api from '../../utils/axios';
+import { LocationsType } from '../Booking';
 import CommonInput from '../Inputs/CommonInput';
 import CommonSelect from '../Selects/CommonSelect';
 import CreatePdf from '../pdf/CreatePdf';
@@ -22,9 +26,11 @@ const dropping_point_list = [
 ]
 
 type PersonalInfoProps = {
-    state: ModalStateType
-    price: string | number
-    setState: React.Dispatch<React.SetStateAction<ModalStateType>>
+    state: ModalStateType;
+    price: string | number;
+    fromID: string | null;
+    coachNo: string;
+    setState: React.Dispatch<React.SetStateAction<ModalStateType>>;
 }
 interface PassengerPersonalInfoState {
     info: {
@@ -40,18 +46,82 @@ interface PassengerPersonalInfoState {
     isLoading: boolean
 }
 
-export default function PersonalInfo({ state, price, setState }: PersonalInfoProps) {
+const PersonalInfo = memo(function PersonalInfo({ state, price, fromID, coachNo, setState }: PersonalInfoProps) {
     const divRef = useRef<HTMLDivElement>(null)
+    const submitButtonRef = useRef<HTMLButtonElement>(null)
     const handlePrint = useReactToPrint({
         content: () => divRef.current,
         documentTitle: 'Ticket.pdf'
-    })
+    });
+    const [locations, setLocations] = useState<{ toLocations: LocationsType[], fromLocations: IdNameBrandLocationFromType[] }>({ fromLocations: [], toLocations: [] });
 
     const { state: { authenticated } } = useContext(Context);
     const [passengerPersonalInfo, setPassengerPersonalInfo] = useState<PassengerPersonalInfoState>({ info: { age: 0, boarding_point: "", dropping_point: "", email: "", gender: "", agree: false, mobile: 0, name: "" }, isLoading: false });
     const [isSubmitted, setSubmitted] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
+
+    // get specific location
+    useEffect(() => {
+        const controller = new AbortController();
+        (async () => {
+            try {
+                const res = await api.get(`location/${fromID}`, { signal: controller.signal });
+
+                setLocations(prev => ({
+                    ...prev,
+                    fromLocations: [res.data],
+                }))
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    const message = error.response?.data?.message;
+
+                    // setToLocations((prev) => ({
+                    //   ...prev,
+                    //   error: message,
+                    // }));
+                    return;
+                }
+                //   setToLocations((prev) => ({
+                //     ...prev,
+                //     error: "Something Went Wrong! Please Try Again.",
+                //   }));
+            }
+        })()
+
+        return () => controller.abort()
+    }, [fromID]);
+
+    // get locations based on from
+    useEffect(() => {
+        const controller = new AbortController();
+        (async () => {
+            try {
+                const res = await api.get(`/search/toLocation/${fromID}`, { signal: controller.signal });
+
+                setLocations(prev => ({
+                    ...prev,
+                    toLocations: res.data,
+                }))
+            } catch (error) {
+                if (axios.isAxiosError(error)) {
+                    const message = error.response?.data?.message;
+
+                    // setToLocations((prev) => ({
+                    //   ...prev,
+                    //   error: message,
+                    // }));
+                    return;
+                }
+                //   setToLocations((prev) => ({
+                //     ...prev,
+                //     error: "Something Went Wrong! Please Try Again.",
+                //   }));
+            }
+        })()
+
+        return () => controller.abort()
+    }, [fromID])
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -77,8 +147,8 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
     }
 
     return (
-        <>
-            <SeatInfo setState={setState} seatNames={state.seatNames} price={price} />
+        <div className='z-50'>
+            <SeatInfo setState={setState} bookingSeats={state.bookingSeats} price={price} />
 
             <form className="mt-3 lg:mt-5 border p-2 rounded-md" onSubmit={handleSubmit}>
                 <p className="text-lg font-bold">Personal Information:</p>
@@ -115,6 +185,7 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
                     <CommonInput
                         name='age'
                         placeholder='00'
+                        minMax={[0, 100]}
                         label='Age'
                         required
                         value={passengerPersonalInfo.info.age}
@@ -139,7 +210,7 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
                         defSelectName="Select Boarding Point"
                         label="Boarding Point"
                         name="boarding_point"
-                        options={boarding_point_list}
+                        options={locations.fromLocations}
                         change={e => onChange(e.target.name, e.target.value)}
                         value={passengerPersonalInfo.info.boarding_point}
                         valueInName
@@ -152,7 +223,7 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
                         defSelectName="Select Dropping Point"
                         label="Dropping Point"
                         name="dropping_point"
-                        options={dropping_point_list}
+                        options={locations.toLocations}
                         change={e => onChange(e.target.name, e.target.value)}
                         value={passengerPersonalInfo.info.dropping_point}
                         valueInName
@@ -185,7 +256,15 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
 
                     <button
                         type="submit"
-                        onClick={handlePrint}
+                        // onClick={() => handlePrint}
+                        onClick={() => {
+                            const { age, agree, boarding_point, dropping_point, email, gender, mobile, name } = passengerPersonalInfo.info
+                            console.log(!age || !agree || !boarding_point || !dropping_point || !email || !gender || !mobile || !name, 'click', passengerPersonalInfo.info)
+                            if (!age || !agree || !boarding_point || !dropping_point || !email || !gender || !mobile || !name) return
+
+                            handlePrint();
+                        }}
+                        ref={submitButtonRef}
                         className="bg-emerald-500 w-[100px] text-white mt-2 active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     >
                         Confirm
@@ -193,8 +272,11 @@ export default function PersonalInfo({ state, price, setState }: PersonalInfoPro
                 </div>
             </form>
 
-            <CreatePdf ref={divRef} />
-
-        </>
+            <div className="hidden">
+                <CreatePdf ref={divRef} state={state} passengerInfo={passengerPersonalInfo.info} coachNo={coachNo} />
+            </div>
+        </div>
     )
-}
+})
+
+export default PersonalInfo
